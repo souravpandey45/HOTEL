2a57750ff1751d576b561c18fe0481d3234a0f9c
sourav <souravpandey4551@gmail.com>
2023-09-24 21:18:47 +0530

    This is hotel booking website

---
+++ HOTEL_ACE/hotel/hotel/home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Amenities)
+admin.site.register(Hotel)
+admin.site.register(HotelImages)
+admin.site.register(HotelBooking)
